a265deb217db4536299b31c7de2889f6ef92cd7f
jakehemmerle <jakehemmerle@protonmail.com>
2023-11-07 18:07:52 -0500

    refactored everything referencing extra to auxilary

--- examples/barebones-with-auxilary/src/lib.rs
+++ examples/barebones-with-auxilary/src/lib.rs
@@ -1,4 +1,4 @@
-//! This example demonstrates a contrieved program that can include extra data. Note, only the data in `preimage` will be signed by Entropy; `extra` is used to provide additional data (eg an additional signature or a zkp related to the preimage) that the user requires during program evaluation.
+//! This example demonstrates a contrieved program that can include auxilary data. Note, only the data in `message` will be signed by Entropy; `auxilary_data` is used to provide additional data (eg an additional signature or a zkp related to the preimage) that the user requires during program evaluation.
 
 #![no_std]
 
@@ -11,9 +11,9 @@ use ec_core::{bindgen::Error, bindgen::*, export_program, prelude::*};
 // TODO confirm this isn't an issue for audit
 register_custom_getrandom!(always_fail);
 
-pub struct BarebonesWithExtra;
+pub struct BarebonesWithAuxilary;
 
-impl Program for BarebonesWithExtra {
+impl Program for BarebonesWithAuxilary {
     /// This is the only function required by the program runtime. `signature_request` includes the message to be
     /// signed, eg. RLP-serialized Ethereum transaction request, raw x86_64 executable, etc.
     fn evaluate(signature_request: SignatureRequest) -> Result<(), Error> {
@@ -29,7 +29,7 @@ impl Program for BarebonesWithExtra {
             ));
         }
 
-        // Just check and make sure the extra field is not empty.
+        // Just check and make sure the `auxilary_data` field is not empty.
         auxilary_data.ok_or(Error::Evaluation(
             "This program requires that `auxilary_data` be `Some`.".to_string(),
         ))?;
@@ -38,43 +38,44 @@ impl Program for BarebonesWithExtra {
     }
 }
 
-export_program!(BarebonesWithExtra);
+export_program!(BarebonesWithAuxilary);
 
-// write a test that calls evaluate and passes it the proper parameters
 #[cfg(test)]
 mod tests {
     use super::*;
     use alloc::vec;
 
     #[test]
-    fn test_preimage_length_is_valid() {
+    fn test_message_length_is_valid() {
         let signature_request = SignatureRequest {
             message: "some_data_longer_than_10_bytes".to_string().into_bytes(),
             auxilary_data: Some(vec![0x00]),
         };
 
-        assert!(BarebonesWithExtra::evaluate(signature_request).is_ok());
+        assert!(BarebonesWithAuxilary::evaluate(signature_request).is_ok());
     }
 
+    /// Note, the program is written s.t. if `message` is less than 10 bytes, the program will error.
     #[test]
-    fn test_preimage_length_is_invalid() {
+    fn test_message_length_is_invalid() {
         let signature_request = SignatureRequest {
             // should error since preimage is less than 10 bytes
             message: "under10".to_string().into_bytes(),
             auxilary_data: Some(vec![0x00]),
         };
 
-        assert!(BarebonesWithExtra::evaluate(signature_request).is_err());
+        assert!(BarebonesWithAuxilary::evaluate(signature_request).is_err());
     }
 
+    /// Note, the program is written s.t. if `auxilary_data` is `None`, the program will error.
     #[test]
-    fn test_error_when_extra_field_is_none() {
+    fn test_error_when_auxilary_field_is_none() {
         let signature_request = SignatureRequest {
             message: "some_data_longer_than_10_bytes".to_string().into_bytes(),
             // should error since auxilary_data field is None
             auxilary_data: None,
         };
 
-        assert!(BarebonesWithExtra::evaluate(signature_request).is_err());
+        assert!(BarebonesWithAuxilary::evaluate(signature_request).is_err());
     }
 }
