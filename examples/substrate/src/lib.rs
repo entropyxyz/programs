9a39ab55c5d3bd9203dffb04a3b999207a1e6f40
Jesse Abramowitz <jesse@entropy.xyz>
2024-07-05 16:19:38 -0400

    add substrate example for pairing

---
+++ examples/substrate/src/lib.rs
@@ -0,0 +1,89 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+extern crate alloc;
+
+use alloc::{
+    format,
+    string::{String, ToString},
+    vec::Vec,
+};
+use entropy_programs::core::{
+    bindgen::*, export_program, prelude::*, SatisfiableForArchitecture, TryParse,
+};
+use entropy_programs_substrate::{
+    check_message_against_transaction, HasFieldsAux, HasFieldsConfig,
+};
+
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+// TODO confirm this isn't an issue for audit
+register_custom_getrandom!(always_fail);
+
+/// JSON-deserializable struct that will be used to derive the program-JSON interface.
+#[cfg_attr(feature = "std", derive(schemars::JsonSchema))]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct UserConfig {
+    max_transfer_amount: u128,
+    genesis_hash: String,
+}
+
+impl HasFieldsConfig for UserConfig {
+    type GenesisHashType = String;
+
+    fn genesis_hash(&self) -> &String {
+        &self.genesis_hash
+    }
+}
+/// JSON representation of the auxiliary data
+#[cfg_attr(feature = "std", derive(schemars::JsonSchema))]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct AuxData {
+    pub spec_version: u32,
+    pub transaction_version: u32,
+    pub string_account_id: String,
+    pub amount: u128,
+}
+
+impl HasFieldsAux for AuxData {
+    type SpecVersionType = u32;
+    type TransactionVersionType = u32;
+    type StringAccountIdType = String;
+    type AmountType = u128;
+
+    fn spec_version(&self) -> &u32 {
+        &self.spec_version
+    }
+
+    fn transaction_version(&self) -> &u32 {
+        &self.transaction_version
+    }
+    fn string_account_id(&self) -> &String {
+        &self.string_account_id
+    }
+    fn amount(&self) -> &u128 {
+        &self.amount
+    }
+}
+
+pub struct SubstrateProgram;
+
+impl Program for SubstrateProgram {
+    fn evaluate(
+        signature_request: SignatureRequest,
+        config: Option<Vec<u8>>,
+        _oracle_data: Option<Vec<u8>>,
+    ) -> Result<(), Error> {
+        check_message_against_transaction::<AuxData, UserConfig>(signature_request, config)
+            .unwrap();
+
+        Ok(())
+    }
+
+    /// Since we don't use a custom hash function, we can just return `None` here.
+    fn custom_hash(_data: Vec<u8>) -> Option<Vec<u8>> {
+        None
+    }
+}
+
+export_program!(SubstrateProgram);
