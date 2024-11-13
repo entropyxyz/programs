4618d44e4f593f7bff3ea4b9f662333b9410d0c3
peg <ameba23@systemli.org>
2024-11-13 12:57:59 +0100

    Update template CLI for latest changes to CLI

--- templates/basic-template/cli/src/main.rs
+++ templates/basic-template/cli/src/main.rs
@@ -1,23 +1,49 @@
 use colored::Colorize;
-use entropy_test_cli::{run_command, PROGRAM_VERSION_NUMBER};
 use dotenv::dotenv;
+use entropy_test_cli::{run_command, PROGRAM_VERSION_NUMBER};
 use generate_types::generate_types;
 use project_root::get_project_root;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv().ok();
-    let program = format!("{}/target/wasm32-unknown-unknown/release/{{project-name}}.wasm", get_project_root()?.to_string_lossy());
+    let program = format!(
+        "{}/target/wasm32-unknown-unknown/release/{{project-name}}.wasm",
+        get_project_root()?.to_string_lossy()
+    );
     generate_types();
-    let config_interface = format!("{}/{{project-name}}_serialized_config_type.txt", get_project_root()?.to_string_lossy());
-    let aux_data_interface = format!("{}/{{project-name}}_serialized_aux_data_type.txt", get_project_root()?.to_string_lossy());
-    match run_command(Some(program.into()), Some(config_interface.into()), Some(aux_data_interface.into()), Some(PROGRAM_VERSION_NUMBER)).await {
+    let config_interface = format!(
+        "{}/{{project-name}}_serialized_config_type.txt",
+        get_project_root()?.to_string_lossy()
+    );
+    let aux_data_interface = format!(
+        "{}/{{project-name}}_serialized_aux_data_type.txt",
+        get_project_root()?.to_string_lossy()
+    );
+
+    let cli = Cli::parse();
+    let json_ouput = cli.json;
+    match run_command(
+        cli,
+        Some(program.into()),
+        Some(config_interface.into()),
+        Some(aux_data_interface.into()),
+        Some(PROGRAM_VERSION_NUMBER),
+    )
+    .await
+    {
         Ok(output) => {
-            println!("Success: {}", output.green());
+            if json_ouput {
+                println!("{}", output);
+            } else {
+                println!("Success: {}", output.green());
+            }
             Ok(())
         }
         Err(err) => {
-            println!("{}", "Failed!".red());
+            if !json_ouput {
+                eprintln!("{}", "Failed!".red());
+            }
             Err(err)
         }
     }
